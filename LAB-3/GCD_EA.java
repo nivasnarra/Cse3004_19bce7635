cc38f04aa485efad785035cc66dbf040bbd9903a
nivasnarra <92721731+nivasnarra@users.noreply.github.com>
2021-11-26 12:17:45 +0530

    Add files via upload

---
+++ LAB-3/GCD_EA.java
@@ -0,0 +1,15 @@
+import java.util.*; 
+import java.lang.*; 
+class Main
+ {
+ public static int gcd(int a, int b) 
+{
+if (a == 0)
+return b;
+ return gcd(b%a, a);
+ }
+ public static void main(String[] args)
+ {
+ System.out.println(gcd(3918848,1653264)); 
+}
+} 
